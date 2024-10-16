5f31784b3b86b9f472390bef87dc1f527e336a25
kenny8222 <8222611@qq.com>
2024-10-16 21:03:51 +0800

    Update diy-part2.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
@@ -14,6 +14,26 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Del duplicate packages------------------
 rm -rf feeds/packages/net/open-app-filter
+#预置HomeProxy数据
+if [ -d *"homeproxy"* ]; then
+	HP_RULES="surge"
+	HP_PATCH="homeproxy/root/etc/homeproxy"
+
+	chmod +x ./$HP_PATCH/scripts/*
+	rm -rf ./$HP_PATCH/resources/*
+
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
+	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+
+	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
+
+	cd .. && rm -rf ./$HP_RULES/
+
+	cd $PKG_PATCH && echo "homeproxy date has been updated!"
+fi
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
